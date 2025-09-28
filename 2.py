5edd05dbbab9d15fb46dc79bca9ee7c1969c81f5
kristina-pachi <kristina.pachi.04@bk.ru>
2025-09-28 13:53:28 +0300

    Постепенная реализация методов

--- 2.py
+++ 2.py
@@ -2,6 +2,7 @@
 # с динамическими скидками (Декоратор + Стратегия)
 import abc
 
+# 1. Компонент (Component): Базовый интерфейс
 class Order(abc.ABC):
     @abc.abstractmethod
     def get_total_cost(self):
@@ -12,6 +13,7 @@ class Order(abc.ABC):
         pass
 
 
+# 2. Конкретный Компонент (Concrete Component)
 class BasicOrder(Order):
 
     def get_total_cost(self):
@@ -21,6 +23,7 @@ class BasicOrder(Order):
         pass
 
 
+# 3. Декоратор (Decorator): Базовый класс
 class OrderDiscountDecorator(Order, abc.ABC):
     def __init__(self, decorated_order):
         self._decorated_order = decorated_order
@@ -34,9 +37,10 @@ class OrderDiscountDecorator(Order, abc.ABC):
         pass
 
 
+# 4. Конкретные Декораторы (Concrete Decorators): Различные
 class  PercentageDiscount(OrderDiscountDecorator):
     def get_total_cost(self):
-        pass
+        return self._decorated_order.get_total_cost()*0.9
 
     def get_description(self):
         pass
@@ -44,7 +48,10 @@ class  PercentageDiscount(OrderDiscountDecorator):
 
 class  FixedAmountDiscount(OrderDiscountDecorator):
     def get_total_cost(self):
-        pass
+        sum =  self._decorated_order.get_total_cost()
+        if sum>=15:
+            return self._decorated_order.get_total_cost()-5
+        return sum
 
     def get_description(self):
         pass
@@ -52,32 +59,58 @@ class  FixedAmountDiscount(OrderDiscountDecorator):
 
 class  LoyaltyDiscount(OrderDiscountDecorator):
     def get_total_cost(self):
-        pass
+        return self._decorated_order.get_total_cost()*0.5
 
     def get_description(self):
         pass
 
 
+# 1. Интерфейс Стратегии (Strategy Interface): Общий интерфейс
 class DeliveryCostStrategy(abc.ABC):
     @abc.abstractmethod
     def calculate_cost(self, distance: float, weight: float):
         pass
 
 
+# 2. Конкретные Стратегии (Concrete Strategies)
 class StandardDelivery(DeliveryCostStrategy):
     def calculate_cost(self, distance: float, weight: float):
-        pass
+        return distance*0.1 + weight*1
 
 
 class ExpressDelivery(DeliveryCostStrategy):
     def calculate_cost(self, distance: float, weight: float):
-        pass
+        return distance*0.2 + weight*2
 
 
 class FreeDeliveryThreshold(DeliveryCostStrategy):
     def calculate_cost(self, distance: float, weight: float):
-        pass
+        return 0
 
 
+# 3. Контекст (Context): Класс, который использует стратегию и декоратор
 class OrderProcessor:
-    pass
+    def __init__(self, distance):
+        self.distance = distance
+        self.items = []
+        self.total_amount = 0.0
+        self.total_weight = 0.0
+        self._delivery_strategy = None
+        self._discount_decorator = None
+
+    def add_item(self, item_name: str, price: float, weight: float):
+        self.items.append((item_name, price, weight))
+        self.total_amount += price
+        self.total_weight += weight
+        print(f"Добавлен товар: {item_name}, Цена: {price} $, Вес: {weight}")
+
+    def set_discount_decorator(self, decorator: OrderDiscountDecorator):
+        self._discount_decorator = decorator
+        print(f"Выбрана скидка: {decorator.__class__.__name__}")
+    
+    def set_delivery_strategy(self, strategy: DeliveryCostStrategy):
+        self._delivery_strategy = strategy
+        print(f"Установлен способ доставки: {strategy.__class__.__name__}")
+    
+    def checkout(self):
+        self._delivery_strategy.calculate_cost(self.total_weight, self.distance)
