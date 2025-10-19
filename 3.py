10d5d4fc488d613af5d9b31640490b6bfd25772c
kristina-pachi <kristina.pachi.04@bk.ru>
2025-10-19 21:47:22 +0300

    Создано окно

---
+++ 3.py
@@ -0,0 +1,10 @@
+# 3 Лабораторная.
+# 4 Задание Интерактивный опросник с Radiobutton и Checkbutton
+import tkinter as tk
+from tkinter import messagebox
+
+
+root = tk.Tk()
+root.title("Важный опрос ʕ ᵔᴥᵔ ʔ")
+root.geometry("600x800")
+
