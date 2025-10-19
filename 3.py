04fd8782609697bd3427f22cac45618bbb149836
kristina-pachi <kristina.pachi.04@bk.ru>
2025-10-19 21:51:34 +0300

    Добавлена метка

--- 3.py
+++ 3.py
@@ -8,3 +8,14 @@ root = tk.Tk()
 root.title("Важный опрос ʕ ᵔᴥᵔ ʔ")
 root.geometry("600x800")
 
+def result():
+    pass
+
+label1 = tk.Label(
+    root,
+    text="Какая вы булочка? (^ ω ^)",
+    font=("Arial", 18), # Установка шрифта и размера
+    fg="red", # Цвет текста (foreground)
+    bg="lightpink" # Цвет фона (background)
+)
+label1.pack(pady=20)
