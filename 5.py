571734f650252e2927315fcb9163eb1025fad82f
kristina-pachi <kristina.pachi.04@bk.ru>
2025-11-14 15:01:38 +0300

    Добавлена функция обзора файлов

--- 5.py
+++ 5.py
@@ -1,28 +1,37 @@
 # 5 Лабораторная.
 # 1 Анализатор текстового файла
+import os
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
+
+
+def choose_file():
+    filepath = filedialog.askopenfilename(
+        title="Выберите файл",
+        filetypes=[("Текстовые файлы", "*.txt")]
+    )
+    if filepath:
+        filename = os.path.basename(filepath)
+        messagebox.showinfo("Выбранный файл", f"Файл выбран: {filename}")
+    else:
+        messagebox.showwarning("Внимание", "Пожалуйста, выбирите текстовый файл.")
+
 
 def result():
-    name_of_file = entry.get()
-    if name_of_file:
-        messagebox.showinfo("Результат", name_of_file)
-    else:
-        messagebox.showwarning("Внимание", "Пожалуйста, введите имя файла.")
+    pass
 
 # --- Интерфейс ---
 root: tk.Tk = tk.Tk()
 root.title("Статистика по файлу")
 root.geometry("300x200")
 
-# Ввод имени файла
-label2: tk.Label = tk.Label(root, text="Введите имя файла:", font=("Arial", 10), fg="blue")
-label2.pack(pady=10)
-entry: tk.Entry = tk.Entry(root, width=30)
-entry.pack(pady=5)
+# Выбор файла
+label: tk.Label = tk.Label(root, text="Выбирите текстовый файл:", font=("Arial", 10), fg="blue")
+label.pack(pady=10)
+
 
 # Кнопка
-button: tk.Button = tk.Button(root, text="Отправить", command=result)
+button: tk.Button = tk.Button(root, text="Обзор файлов", command=choose_file)
 button.pack(pady=5)
 
 root.mainloop()
