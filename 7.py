cea15706a17256e8dea4fa414b5887f08819c58d
kristina-pachi <kristina.pachi.04@bk.ru>
2025-12-13 16:03:22 +0300

    Добавлен input на ник

--- 7.py
+++ 7.py
@@ -20,9 +20,7 @@ class Game:
         if player_choice == choice:
             print('Поздравляю, ты угадал!')
             self.score += 1
-            if self.score == 3:
-                self.level += 1
-                self.score = 0
+            self.level = self.score//3
         else:
             print('Промах, попробуй ещё раз')
     
@@ -39,7 +37,7 @@ class Game:
             return pickle.load(f)
     
     def __str__(self):
-        return f'Игрок {self.player_name} заработал {self.score} очков и имеет уровень {self.level}'
+        return f'У игрока {self.player_name} счёт - {self.score} и уровень - {self.level}'
 
 
 # разделение на новою игру и загрузку сохранений
@@ -48,13 +46,8 @@ if os.path.exists("game_data.pkl"):
     print("Загружена сохранённая игра:")
     print(player)
 else:
-    player = Game('@user345')
+    player = Game(input('Ваш ник: '))
     print("Начата новая игра.")
 
 player.play()
 player.save_game()
-
-loaded_game = Game.load_game()
-if loaded_game:
-    print("Состояние после сохранения и загрузки:")
-    print(loaded_game)
