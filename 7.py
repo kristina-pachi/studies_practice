1cd0ae85120fb8df60975e97ab51b61f8c335e99
kristina-pachi <kristina.pachi.04@bk.ru>
2025-12-13 15:38:46 +0300

    Добавлено разделение на новою игру и продолжение

--- 7.py
+++ 7.py
@@ -3,15 +3,14 @@
 
 import pickle
 import random
+import os
 
-
-
-class Game():
-    def __init__(self, player_name):
+class Game:
+    def __init__(self, player_name, filename='game_data.pkl'):
         self.player_name = player_name
         self.score = 0
         self.level = 0
-        self.filename = 'game_data.pkl'
+        self.filename = filename
     
     def play(self):
         nums = [random.randint(1, 100) for _ in range(3)]
@@ -21,32 +20,41 @@ class Game():
         if player_choice == choice:
             print('Поздравляю, ты угадал!')
             self.score += 1
-            if self.score == 10:
+            if self.score == 3:
                 self.level += 1
                 self.score = 0
         else:
-            print('Промах, попробуй ёще раз')
-
-
+            print('Промах, попробуй ещё раз')
+    
     def save_game(self):
         with open(self.filename, 'wb') as f:
             pickle.dump(self, f)
-
-
+    
     @staticmethod
     def load_game(filename='game_data.pkl'):
+        if not os.path.exists(filename):
+            print("Нет сохранённой игры.")
+            return None
         with open(filename, 'rb') as f:
             return pickle.load(f)
-
     
     def __str__(self):
         return f'Игрок {self.player_name} заработал {self.score} очков и имеет уровень {self.level}'
 
 
-player = Game('@user345')
+# разделение на новою игру и загрузку сохранений
+if os.path.exists("game_data.pkl"):
+    player = Game.load_game()
+    print("Загружена сохранённая игра:")
+    print(player)
+else:
+    player = Game('@user345')
+    print("Начата новая игра.")
 
 player.play()
 player.save_game()
 
 loaded_game = Game.load_game()
-print(loaded_game)
+if loaded_game:
+    print("Состояние после сохранения и загрузки:")
+    print(loaded_game)
